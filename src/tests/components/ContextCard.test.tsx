9c66b4dec51bdfa5c4b16a72c5ab7362b3e6933c
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-05-22 09:49:53 +0100

    test: add test for ContextCard

---
+++ src/tests/components/ContextCard.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import ContextCard from '../../components/ContextCard'
+
+describe('ContextCard', () => {
+  test('renders the ContextCard component correctly with its heading', () => {
+    render(<ContextCard text="Test Context" />)
+
+    const headingElement = screen.getByRole('heading', { name: /context/i })
+    expect(headingElement).toBeInTheDocument()
+  })
+
+  test('renders the correct text passed as a prop', () => {
+    const testText = 'Some text'
+    render(<ContextCard text={testText} />)
+
+    const textElement = screen.getByRole('paragraph')
+
+    expect(textElement).toBeInTheDocument()
+    expect(textElement).toHaveTextContent(testText)
+  })
+})
