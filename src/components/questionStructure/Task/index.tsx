77282b4948bdca762ba807dfe176a1b09fb67a92
Kishan Sambhi <kss22@ic.ac.uk>
2024-07-16 16:36:20 +0100

    refactor: rename MATHS_SINGLE_ANSWER to PROCESSD_HANDWRITING

--- src/components/questionStructure/Task/index.tsx
+++ src/components/questionStructure/Task/index.tsx
@@ -36,8 +36,8 @@ const taskComponentMap = {
   [TaskType.FLAG]: FlagTask,
   [TaskType.MULTIPLE_CHOICE_SELECT_ONE]: MCQOneTask,
   [TaskType.MULTIPLE_CHOICE_SELECT_SEVERAL]: MCQMultiTask,
-  [TaskType.MATHS_SINGLE_ANSWER]: MathsSingleAnswerTask,
   [TaskType.HANDWRITING]: HandwritingTask,
+  [TaskType.PROCESSED_HANDWRITING]: HandwritingTask,
 } as const
 
 type TaskComponent = FC<TaskProps & { instructions?: string }>
