8e018ef5b988b464f6214389ae7b916166aab3bf
Kishan Sambhi <kss22@ic.ac.uk>
2024-07-19 11:46:16 +0100

    refactor: move ConfirmDialog out of Canvas & add tests

--- src/components/questionStructure/Task/variants/HandwritingTask/Canvas.tsx
+++ src/components/questionStructure/Task/variants/HandwritingTask/Canvas.tsx
@@ -11,7 +11,7 @@ import React, {
   useState,
 } from 'react'
 
-import { ConfirmDialog } from './ConfirmDialog'
+import { ConfirmDialog } from '../../../../ConfirmDialog'
 import useLiveUpdates from './live-updates.hook'
 
 const stopEvent = (e: SyntheticEvent | Event) => {
