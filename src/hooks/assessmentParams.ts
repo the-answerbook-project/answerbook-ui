ddee5eabdcc3a788d02d287fabaf4b781e246609
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-08-01 12:50:34 +0200

    refactor: remove spurious log

--- src/hooks/assessmentParams.ts
+++ src/hooks/assessmentParams.ts
@@ -2,7 +2,6 @@ import { useParams } from 'react-router-dom'
 
 export const useAssessmentParams = () => {
   const { year, moduleCode, qualifier } = useParams()
-  console.log(year, moduleCode, qualifier)
   return {
     assessmentID: `y${year}_${moduleCode}_${qualifier}`,
   }
