fc0a32860e661ffab80a7ef076af6bc7aa950dfc
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-08-21 17:37:25 +0100

    feat: separate handling of task input from answer update for MCQOne

--- src/components/questionStructure/Task/variants/MCQ.tsx
+++ src/components/questionStructure/Task/variants/MCQ.tsx
@@ -3,7 +3,6 @@ import { isEqual, map } from 'lodash'
 import React, { FC, useEffect, useState } from 'react'
 
 import { TaskType } from '../constants'
-import { defaultOnChangeHandler } from '../index'
 import { TaskBaseProps } from '../types'
 
 type MCQOption = {
@@ -27,13 +26,18 @@ export const MCQOneTask: FC<MCQOneTaskProps> = ({
   choices,
   disabled = false,
 }) => {
+  const [value, setValue] = useState(answer)
+  useEffect(() => {
+    if (value !== undefined) onAnswerUpdate(value)
+  }, [value, onAnswerUpdate])
+
+  const handleOnClick = (e) => {
+    const newValue = e.target.value
+    setValue(newValue)
+  }
+
   return (
-    <RadioGroup.Root
-      variant="soft"
-      disabled={disabled}
-      value={answer}
-      onClick={defaultOnChangeHandler(onAnswerUpdate)}
-    >
+    <RadioGroup.Root variant="soft" disabled={disabled} value={value} onClick={handleOnClick}>
       {map(choices, (o) => (
         <RadioGroup.Item key={o.value} value={o.value}>
           {o.label}
