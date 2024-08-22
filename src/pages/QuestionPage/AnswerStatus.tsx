05de2bfd46b228516d1bd0007614eb229912f732
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-08-22 16:14:10 +0100

    feat: update last updated every 10s

--- src/pages/QuestionPage/AnswerStatus.tsx
+++ src/pages/QuestionPage/AnswerStatus.tsx
@@ -1,19 +1,37 @@
 import { CheckIcon } from '@radix-ui/react-icons'
 import { Text } from '@radix-ui/themes'
 import { formatDistanceToNow } from 'date-fns'
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 interface AnswerStatusProps {
   timestamp?: Date
 }
 
 const AnswerStatus: FC<AnswerStatusProps> = ({ timestamp }) => {
-  const formattedTimestamp = timestamp
-    ? formatDistanceToNow(timestamp, {
-        includeSeconds: true,
-        addSuffix: true,
-      })
-    : 'never'
+  const [formattedTimestamp, setFormattedTimestamp] = useState('')
+
+  useEffect(() => {
+    if (timestamp) {
+      const updateFormattedTimestamp = () => {
+        setFormattedTimestamp(
+          formatDistanceToNow(timestamp, {
+            includeSeconds: true,
+            addSuffix: true,
+          })
+        )
+      }
+
+      // Update the timestamp immediately when the component mounts
+      updateFormattedTimestamp()
+
+      // Set up an interval to update the timestamp every 10 seconds
+      const intervalId = setInterval(updateFormattedTimestamp, 10000)
+
+      // Clean up the interval when the component unmounts
+      return () => clearInterval(intervalId)
+    } else setFormattedTimestamp('never')
+  }, [timestamp])
+
   return (
     <>
       <CheckIcon color="green" />
