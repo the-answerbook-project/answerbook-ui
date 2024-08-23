e0d27e2fc28e9016ddbc3c30aec67230d998270a
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-08-23 18:03:16 +0100

    refactor: reinstate tests after adjusting to new code

--- src/tests/components/questionStructure/Task.test.tsx
+++ src/tests/components/questionStructure/Task.test.tsx
@@ -1,36 +1,44 @@
 import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+
+import { TaskFactory } from '../../../components/questionStructure/Task'
+import { TaskType } from '../../../components/questionStructure/Task/constants'
+import { EssayTask } from '../../../components/questionStructure/Task/variants/EssayTask'
+import { Answer } from '../../../types/exam'
 
 jest.mock('../../../components/questionStructure/Task/variants/EssayTask', () => ({
   EssayTask: jest.fn(),
 }))
-//
-// describe('Task Component', () => {
-//   afterEach(jest.clearAllMocks)
-//   const updateHandler = jest.fn()
-//   it('should render correctly according to requested task type', () => {
-//     render(
-//       <TaskFactory type={TaskType.ESSAY} answer="Test answer" onAnswerUpdate={updateHandler} />
-//     )
-//     expect(EssayTask).toHaveBeenCalledWith(
-//       expect.objectContaining({
-//         type: TaskType.ESSAY,
-//         answer: 'Test answer',
-//         onAnswerUpdate: updateHandler,
-//       }),
-//       {}
-//     )
-//   })
-//
-//   it('renders the description if provided', () => {
-//     render(
-//       <TaskFactory
-//         instructions="Test description"
-//         type={TaskType.ESSAY}
-//         answer="Test answer"
-//         onAnswerUpdate={updateHandler}
-//       />
-//     )
-//     const descriptionElement = screen.getByTestId('markdown')
-//     expect(descriptionElement).toBeInTheDocument()
-//   })
-// })
+
+const answer = {
+  answer: 'Test answer',
+} as Answer
+
+describe('Task Component', () => {
+  afterEach(jest.clearAllMocks)
+  const updateHandler = jest.fn()
+  it('should render correctly according to requested task type', () => {
+    render(<TaskFactory type={TaskType.ESSAY} answer={answer} onAnswerUpdate={updateHandler} />)
+    expect(EssayTask).toHaveBeenCalledWith(
+      expect.objectContaining({
+        type: TaskType.ESSAY,
+        answer: answer,
+        onAnswerUpdate: updateHandler,
+      }),
+      {}
+    )
+  })
+
+  it('renders the description if provided', () => {
+    render(
+      <TaskFactory
+        instructions="Test description"
+        type={TaskType.ESSAY}
+        answer={answer}
+        onAnswerUpdate={updateHandler}
+      />
+    )
+    const descriptionElement = screen.getByTestId('markdown')
+    expect(descriptionElement).toBeInTheDocument()
+  })
+})
